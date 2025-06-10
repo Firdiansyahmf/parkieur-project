27856fbb6ec9d47e63b98b2e187eafa82095cf90
Firdiansyahmf <firdiansyahmf.04@gmail.com>
2025-06-10 17:16:58 +0700

    Tambah menuUtama()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Supaya kodenya auto clear (biar rapih)
+// fungsi clearScreen
 #ifdef _WIN32
     #define CLEAR_COMMAND "cls"
 #else 
@@ -11,7 +11,37 @@ void clearScreen() {
     system(CLEAR_COMMAND);
 };
 
+// menuUtama
+void menuUtama() {
+    string pilihan;
+    do {
+        cout << "\n--- SISTEM PARKIR BASEMENT MALL ---\n";
+        cout << "1. Kendaraan Masuk\n";
+        cout << "2. Kendaraan Keluar\n";
+        cout << "3. Tampilkan Parkir\n";
+        cout << "4. Pengaturan\n";
+        cout << "0. Keluar\n";
+        cout << "Pilih: ";
+        cin >> pilihan;
+
+        if (pilihan == "1") {
+            // tambahKendaraan();
+        } else if (pilihan == "2") {
+        } else if (pilihan == "3") {
+            // tampilkanParkir();
+        } else if (pilihan == "4") {
+            // pengaturan();
+        } else if (pilihan == "0") {
+            cout << "Keluar dari program.\n";
+        } else {
+            cout << "Pilihan tidak valid. Silakan pilih antara 0 - 4.\n";
+        }
+
+    } while (pilihan != "0");
+};
+
 int main()
 {
+    menuUtama();
     return 0;
 }
