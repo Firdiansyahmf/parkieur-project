ef9508b4faa256b2c04248cddf9edc56ee24a25b
Zsxam <gb11102005@gmail.com>
2025-06-10 15:53:50 +0700

    Tambah fungsi auto clean

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// Supaya kodenya auto clear (biar rapih)
+#ifdef _WIN32
+    #define CLEAR_COMMAND "cls"
+#else 
+    #define CLEAR_COMMAND "clear"
+#endif
+void clearScreen() {
+    system(CLEAR_COMMAND);
+};
+
 int main()
 {
     return 0;
